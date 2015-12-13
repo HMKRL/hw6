a8390602b776152677b8c72cc83ade0b9af9ddf8
Yu-Cheng Liang <a135246a135246@gmail.com>
2015-12-14 02:20:12 +0800

    Both OK

--- hw6_1.c
+++ hw6_1.c
@@ -7,12 +7,9 @@ void print_bit_double(long*);
 int main()
 {
 	int i;
-	char c[32];
-	struct {
-		unsigned int Field : 32;
-	} AT;
+	char fc[32], dc[64];
 	float fnum, *fpt;
-	double dnum;
+	double dnum, *dpt;
 	int *ipt;//sizeof(float) == 4 == sizeof(int), use int pointer
 	long *lpt;//sizeof(double) == 8 == siezof(long), use long pointer
 	printf("Input the float number:");
@@ -21,22 +18,31 @@ int main()
 	print_bit_float(ipt);
 
 	printf("Input binary to convert to float:");
-	fpt = &AT;
 	unsigned int a = 0;
-	scanf("%s",c);
+	scanf("%s",fc);
 	for(i = 31;i >= 0;i--)
 	{
-		if(c[31 - i] == '1')
+		if(fc[31 - i] == '1')
 			a |= (1 << i);
-		else
-			a |= (0 << i);
 	}
 	fpt = &a;
 	printf("%e\n", *fpt);
+
 	printf("Input the double number:");
 	scanf("%lf",&dnum);
 	lpt = &dnum;
 	print_bit_double(lpt);
+
+	printf("Input binary to convert to double:");
+	unsigned long d = 0;
+	scanf("%s",dc);
+	for(i = 63;i >= 0;i--)
+	{
+		if(dc[63 - i] == '1')
+			d |= ((long)1 << i);
+	}
+	dpt = &d;
+	printf("%e\n", *dpt);
 
 	return 0;
 }
